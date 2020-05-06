bcf78c5552393191ee0d9762088a6a89be8d2d9a
Travis <trajamsmith@gmail.com>
2020-05-06 15:10:21 -0400

    Yuck

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Atari RL
 Exploring a now-classic problem in reinforcement learning — training an agent to play Atari games.
+
+## Installation steps for Windows
+Follow this for `gym` on Windows: https://towardsdatascience.com/how-to-install-openai-gym-in-a-windows-environment-338969e24d30
--- gym/gym/spaces/space.py
+++ gym/gym/spaces/space.py
@@ -6,8 +6,10 @@ class Space(object):
     code that applies to any Env. For example, you can choose a random
     action.
     """
+
     def __init__(self, shape=None, dtype=None):
         import numpy as np  # takes about 300-400ms to import, so we load lazily
+
         self.shape = None if shape is None else tuple(shape)
         self.dtype = None if dtype is None else np.dtype(dtype)
         self.np_random = None
